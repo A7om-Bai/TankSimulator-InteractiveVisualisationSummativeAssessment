d2ff565f13292f2224dbe6a490943af24581f85d
A7om <18227396650@163.com>
2025-12-05 21:17:58 +0000

    FinalMenuUpdate

--- Assets/Scripts/BackgroundMusicPlayerController.cs
+++ Assets/Scripts/BackgroundMusicPlayerController.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundMusicPlayerController : MonoBehaviour
 {
     private void Awake()
     {
-        if(FindObjectsOfType<BackgroundMusicPlayerController>().Length > 1)
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == "03Garage")
+        {
+            var all = FindObjectsOfType<BackgroundMusicPlayerController>();
+            foreach (var bgm in all)
+            {
+                if (bgm != this && bgm.gameObject.scene.name == "DontDestroyOnLoad")
+                {
+                    Destroy(bgm.gameObject);
+                }
+            }
+            return;
+        }
+
+        var existing = FindObjectsOfType<BackgroundMusicPlayerController>();
+
+        if (existing.Length > 1)
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Menu : MonoBehaviour
 {
+    public AudioSource clickSound;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("02MainMap");
@@ -10,6 +13,16 @@ public class Menu : MonoBehaviour
 
     public void Garage()
     {
+        StartCoroutine(LoadGarage());
+    }
+
+    IEnumerator LoadGarage()
+    {
+        if (clickSound != null)
+            clickSound.Play();
+
+        yield return new WaitForSeconds(0.1f);
+
         SceneManager.LoadScene("03Garage");
     }
 }
